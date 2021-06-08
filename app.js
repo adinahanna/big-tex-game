01472165e39c90fee27a9a59dcf8eb18a21519c7
adinahanna <adina.weinberg@gmail.com>
2021-06-08 09:53:12 -0500

    complete fair-goer update function

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+// GLOBAL VARS 
+const 
+const fairGoerCounter = document.querySelector('#fair-goer-counter');
+
+// FUNCTIONS
+// start game function
+//on click:
+// fair-goers number gets assigned
+// attendee counter populates
+// card deck is shuffled and images placed
+
+// randomly assign number of fair-goers
+//console.log(fairGoerCounter);
+const fairGoerArr= [3, 4, 5, 6, 7, 8];
+const updateFairGoers = () => {
+  const randomIndex = Math.floor(Math.random() * fairGoerArr.length);
+  //console.log(fairGoerArr[randomIndex]);
+  const randomFairGoers = (fairGoerArr[randomIndex]);
+  fairGoerCounter.innerHTML = `
+  <h4>FAIR-GOER COUNTER</h4>
+  <h5>There are ${randomFairGoers} people at the fair today!</h5>
+  `
+};
+updateFairGoers();
+
+
+    // populate cowboy emojis based on number of fair-goers
+    // shuffle card deck and assign images randomly to card divs
+    // update results - incorrect or correct match
+    // check for win or lose: after each match check if correct matches = number of fair-goers and if number of incorrect matches = 3
+    // update big tex image: mvp: changes to different colored big tex on lose; stretch: tints darker red on each incorrect match
+
+
+
+// MODAL FUNCTIONALITY
 // Get the modal
 const modal = document.getElementById("modal");
 
@@ -26,17 +61,5 @@ window.onclick = function(event) {
   }
 };
 
-// FUNCTIONS
-    // start game function
-        //on click:
-            // fair-goers number gets assigned
-            // attendee counter populates
-            // card deck is shuffled and images placed
-    // randomly assign number of fair-goers
-    // populate cowboy emojis based on number of fair-goers
-    // shuffle card deck and assign images randomly to card divs
-    // check if match
-    // update results - incorrect or correct match
-    // check for win or lose: after each match check if correct matches = number of fair-goers and if number of incorrect matches = 3
-    // update big tex image: mvp: changes to different colored big tex on lose; stretch: tints darker red on each incorrect match
+
 
