490f1d3d2f325162c08ba5e4fde2a94f77ea30c2
adinahanna <adina.weinberg@gmail.com>
2021-06-08 20:17:05 -0500

    complete shuffle, fair visitor, and populate card images functions

--- app.js
+++ app.js
@@ -1,34 +1,93 @@
 // GLOBAL VARS 
-const 
+const startBtn = document.querySelector('#startBtn');
 const fairGoerCounter = document.querySelector('#fair-goer-counter');
+const fairGoerArr= [3, 4, 5, 6, 7, 8];
+const gameBoard = document.querySelector('.board-flex-container');
+
 
 // FUNCTIONS
 // start game function
-//on click:
-// fair-goers number gets assigned
-// attendee counter populates
-// card deck is shuffled and images placed
+const handleStartBtnClick = event => {
+  event.preventDefault;
+// fair-goers number gets assigned and attendee counter populates
+  updateFairGoers();
+// card deck is shuffled 
+//images placed
+  addImages();
+};
+
+startBtn.addEventListener('click', handleStartBtnClick);
 
 // randomly assign number of fair-goers
-//console.log(fairGoerCounter);
-const fairGoerArr= [3, 4, 5, 6, 7, 8];
 const updateFairGoers = () => {
   const randomIndex = Math.floor(Math.random() * fairGoerArr.length);
   //console.log(fairGoerArr[randomIndex]);
   const randomFairGoers = (fairGoerArr[randomIndex]);
   fairGoerCounter.innerHTML = `
-  <h4>FAIR-GOER COUNTER</h4>
-  <h5>There are ${randomFairGoers} people at the fair today!</h5>
+  <h4>FAIR VISITORS</h4>
+  <p>There are ${randomFairGoers} people at the fair today. Make ${randomFairGoers} matches.</p>
   `
 };
-updateFairGoers();
+//create card img array
+const cardArr = ['images/dallas-skyline.jpeg', 'images/dr-pepper.png', 'images/eggs.png', 'images/ferris-wheel.png', 'images/fletchers.png', 'images/flour.jpeg', 'images/milk.png', 'images/hat.png'];
+const doubledCardArr = [...cardArr, ...cardArr];
+ 
+// shuffle card deck - Fisher-Yates shuffle
+function shuffle(array) {
+  var currentIndex = array.length,  randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+};
+
+//assign images randomly to card divs
+const addImages = () => {
+  shuffle(doubledCardArr);
+  const cardElements = document.querySelectorAll('.card');
+  for (let i = 0; i < doubledCardArr.length; i++) {
+    //let image = cardArr[i].value;
+    // create a new div
+    const cardDiv = document.createElement('div');
+    // use css to update background image
+    cardDiv.setAttribute('id', doubledCardArr[i]);
+    cardDiv.style.backgroundImage = `url('${doubledCardArr[i]}'`;
+    // add class with display: none
+    cardDiv.setAttribute('class', 'cardDisplay');
+    cardDiv.classList.add('hide');
+    // append the div to the container
+    cardElements[i].appendChild(cardDiv);
+  }
+};
+//have them show on click
+const matchCards = (event) => {
+  event.preventDefault;
+  if (event.target.classList.contains('card')){
+    event.target.querySelector('.cardDisplay').classList.remove('hide');
+  }
+};
+gameBoard.addEventListener('click', matchCards);
+
+//check if match
+const checkIfMatch = (event) => {
+  
+
+  
+
+};
+gameBoard.addEventListener('click', checkIfMatch);
 
 
-    // populate cowboy emojis based on number of fair-goers
-    // shuffle card deck and assign images randomly to card divs
-    // update results - incorrect or correct match
-    // check for win or lose: after each match check if correct matches = number of fair-goers and if number of incorrect matches = 3
-    // update big tex image: mvp: changes to different colored big tex on lose; stretch: tints darker red on each incorrect match
+// populate cowboy emojis based on number of fair-goers
+// update results - incorrect or correct match
+// check for win or lose: after each match check if correct matches = number of fair-goers and if number of incorrect matches = 3
+// update big tex image: mvp: changes to different colored big tex on lose; stretch: tints darker red on each incorrect match
 
 
 
